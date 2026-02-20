b65067a1a5d894ae16faf52cbdadf724a3834581
Ganesh0690 <ganirider4@gmail.com>
2026-02-20 14:54:53 +0530

    Fix WASM build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
+  optimizeDeps: {
+    exclude: ["@provablehq/wasm"],
+  },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
